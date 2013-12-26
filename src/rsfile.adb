ffe9d7df35bb8f8c941394b2333ac13679b33cd6
Alejandro R. Mosteo <alejandro@mosteo.com>
2013-12-26 03:45:05 +0100

    RC 1

--- src/rsfile.adb
+++ src/rsfile.adb
@@ -2,6 +2,7 @@ with Ada.Command_Line;
 with Ada.Containers.Indefinite_Ordered_Maps;
 with Ada.Directories;
 with Ada.Exceptions;
+with Ada.Numerics.Discrete_Random;
 with Ada.Strings.Unbounded; 	use Ada.Strings.Unbounded;
 with Ada.Text_IO;
 
@@ -17,7 +18,8 @@ procedure Rsfile is
    function "+" (S : String) return Unbounded_String
                  renames To_Unbounded_String;
 
-   procedure Log (S : String) renames Ada.Text_IO.Put_Line;
+   --  procedure Log (S : String) renames Ada.Text_IO.Put_Line;
+   procedure Log (S : String) is null;
 
    use type Ada.Directories.File_Size;
    package Size_File is new Ada.Containers.Indefinite_Ordered_Maps
@@ -35,7 +37,7 @@ procedure Rsfile is
       use Ada.Command_Line;
       use Ada.Text_IO;
    begin
-      Put_Line ("Usage: " & Command_Name & " <path>");
+      Put_Line ("Usage: " & Ada.Directories.Simple_Name (Command_Name) & " <path>");
    end Print_Usage;
 
 ----------------------
@@ -61,14 +63,49 @@ procedure Rsfile is
    procedure Populate_Tree is
       use Ada.Directories;
       Root : String := To_String (Rsfile.Root);
-      St   : Search_Type;
-   begin
-      Start_Search (St, Root, "");
+      Pos  : File_Size := 0;
 
-   exception
-      when others =>
-         End_Search (St);
+      ----------------------
+      -- Enumerate_Folder --
+      ----------------------
+
+      procedure Enumerate_Folder (Folder : String) is
+         St   : Search_Type;
+      begin
+         Log ("Entering: " & Folder);
+         Start_Search (St, Folder, "");
+
+         while More_Entries (St) loop
+            declare
+               Item : Directory_Entry_Type;
+            begin
+               Get_Next_Entry (St, Item);
+
+               case Kind (Item) is
+                  when Directory =>
+                     if
+                       Simple_Name (Item) /= "." and then
+                       Simple_Name (Item) /= ".."
+                     then
+                        Enumerate_Folder (Full_Name (Item));
+                     end if;
+                  when Ordinary_File =>
+                     Pos := Pos + Size (Item) + 1;
+                     Log ("Insert: " & Simple_Name (Item) & " at" & Pos'Img);
+                     Candidates.Insert (Pos, Full_Name (Item));
+                  when others =>
+                     Log ("Skipping: " & Simple_Name (Item));
+               end case;
+            end;
+         end loop;
+      exception
+         when others =>
+           End_Search (St);
          raise;
+      end Enumerate_Folder;
+
+   begin
+      Enumerate_Folder (Root);
    end Populate_Tree;
 
 ---------------
@@ -76,8 +113,23 @@ procedure Rsfile is
 ---------------
 
    procedure Pick_File is
+      use Ada.Directories;
+      subtype Targets is File_Size range 0 .. Candidates.Last_Key;
+      package FSRnd is new Ada.Numerics.Discrete_Random (Targets);
+      Rnd : FSRnd.Generator;
    begin
-      null;
+      FSRnd.Reset (Rnd);
+
+      declare
+         Index  : constant File_Size := FSRnd.Random (Rnd);
+
+         Target : constant String :=
+           Size_File.Element
+             (Candidates.Ceiling
+                  (Index));
+      begin
+         Ada.Text_IO.Put_Line (Target);
+      end;
    end Pick_File;
 
    use Gnat.OS_Lib;
